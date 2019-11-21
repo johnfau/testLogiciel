c49ac73de80cbb53d2bfccd3e9adbec58d4edacd
John Fa <jonathan.faucher@gmail.com>
2019-11-21 08:49:29 +0100

    Ajouter une propriété

--- src/main/java/fr/isis/testgeniegitlog/NewClass.java
+++ src/main/java/fr/isis/testgeniegitlog/NewClass.java
@@ -10,5 +10,19 @@ package fr.isis.testgeniegitlog;
  * @author John
  */
 public class NewClass {
-    
+    private String nom;
+
+    /**
+     * @return the nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * @param nom the nom to set
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
